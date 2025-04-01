9b38d2bde8922aa1fdaafa776951716067d7e8df
Ahmed Galal <ahmedgalal11045@gmail.com>
2025-04-01 03:01:49 +0200

    Changed the Secret key from the default one cuz im a dummy dumm dum

--- backend/language_learning_backend/settings.py
+++ backend/language_learning_backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-271kr6tkn2zx-ccs59d__l&g-k-xvpgqt)5e%8nlue$jr&8+tu')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
